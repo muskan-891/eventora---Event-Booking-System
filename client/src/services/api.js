b0ca859839537a4a22d15a6e079166133e661299
Muskan-13-ms <muskansaluja.ms@gmail.com>
2026-09-20 16:24:53 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://eventora-backend-26w1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
